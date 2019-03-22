4130924c5cc621066732eea6ff097d9b34adcb44
lauramann <lauramann@trentu.ca>
2019-03-21 20:06:39 -0400

    Updates

---
+++ precache-manifest.6d4fc4e515c0473ef684ae55c5e7cf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed5abf4070c5adbc9f0",
+    "url": "/public/static/css/main.cd0ff91f.chunk.css"
+  },
+  {
+    "revision": "8ed5abf4070c5adbc9f0",
+    "url": "/public/static/js/main.8ed5abf4.chunk.js"
+  },
+  {
+    "revision": "e96f2f074e0ed9e321c2",
+    "url": "/public/static/js/runtime~main.e96f2f07.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/public/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "e5fb0e50d98e5e8fed96882b993dd794",
+    "url": "/public/static/media/Group.e5fb0e50.png"
+  },
+  {
+    "revision": "28c4e626cc196cd5748e26ad112e8458",
+    "url": "/public/index.html"
+  }
+];
